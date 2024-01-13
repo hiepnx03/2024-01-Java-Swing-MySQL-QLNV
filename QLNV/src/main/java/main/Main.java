730202bbc0ebf0009a181d955b1326b400daffaf
hiepnx03 <hiepnx03@gmail.com>
2024-01-14 02:10:39 +0700

    upload

---
+++ QLNV/src/main/java/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+import view.FormDangNhap;
+
+public class Main {
+
+    public static void main(String[] args) {
+        FormDangNhap formDangNhap = new FormDangNhap();
+        formDangNhap.show();
+    }
+}
